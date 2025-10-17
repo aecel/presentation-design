d28f4a61117fa68fa279ceffe99637e7bd5f40bf
aecel <aecelacuna@gmail.com>
2025-10-17 19:53:40 +0800

    Changed links and email

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -57,11 +57,11 @@ const ContactMe = ({ triggerRef, triggerRef2 }) => {
         </div>
         <a
           id="HireMeButton"
-          href=" mailto:sniffysloth@gmail.com?subject=Hi%20Ace%2C%20I%20would%20love%20to%20hire%20you.&body=I%20saw%20your%20portfolio%20on%20aecel.github.io%2Fpresentation-design.%0D%0A%0D%0ALet%20me%20introduce%20myself%20%2F%20my%20company.%0D%0A%0D%0A%0D%0A "
+          href="https://calendly.com/acunapresentations"
         >
           Hire Me
         </a>
-        <div>Email: sniffysloth@gmail.com</div>
+        <div>Email: acunapresentations@gmail.com</div>
       </div>
       {/* <WavesVideo /> */}
     </section>
